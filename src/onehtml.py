04b32c803e7e8251bcaa2f1b19326e096d6122c7
btonasse <33516406+btonasse@users.noreply.github.com>
2022-03-17 17:29:50 +0100

    venv

--- src/onehtml.py
+++ src/onehtml.py
@@ -1,13 +1,65 @@
 import base64
 from pathlib import Path
 import re
-from typing import Dict
+from typing import Set, Dict, List
+
 
 class onehtml:
     @staticmethod
-    def get_all_images_in_folder(folder: Path) -> Dict[Path, Path]:
+    def get_all_images_in_folder(folder: Path | str) -> Dict[Set[Path], Set[Path]]:
         """
         Return a dictionary containing two lists of paths: one for png files and one for jpg/jpeg.
         """
-        raise NotImplementedError
+        if isinstance(folder, str):
+            folder = Path(folder)
 
+        if not folder.is_dir():
+            raise TypeError(f"{folder.as_posix()} is not a directory.")
+
+        return_dict = {"png": set(), "jpg": set()}
+
+        for item in folder.iterdir():
+            if item.suffix == ".png":
+                return_dict["png"].add(item)
+            elif item.suffix in (".jpg", ".jpeg"):
+                return_dict["jpg"].add(item)
+
+        return return_dict
+
+    @staticmethod
+    def find_all_img_paths_in_html(file_path: Path | str) -> List[str]:
+        if isinstance(file_path, str):
+            file_path = Path(file_path)
+        if not file_path.suffix in (".html", ".htm"):
+            raise TypeError(f"{file_path.as_posix()} is not a HTML file.")
+
+        img_tax_regex = re.compile(r"<img[^>]*src=\"([^\"]+)\"[^>]*>")
+        with file_path.open(encoding="utf-8") as html:
+            return re.findall(img_tax_regex, html.read())
+
+    @staticmethod
+    def get_dict_of_img_paths(
+        img_paths: List[str], base_path: Path
+    ) -> Dict[Set[Path], Set[Path]]:
+        return_dict = {"png": set(), "jpg": set()}
+
+        for item in img_paths:
+            if not item.startswith("http"):
+                if item.startswith("/"):
+                    raise NotImplementedError("Need to implement domain-relative path")
+                else:
+                    if item.endswith(".png"):
+                        return_dict["png"].add(Path(base_path) / Path(item))
+                    elif item.endswith(".jpg") or item.endswith(".jpeg"):
+                        return_dict["jpg"].add(Path(base_path) / Path(item))
+            else:
+                raise NotImplementedError("Need to implement http paths")
+        return return_dict
+
+    @classmethod
+    def get_all_images_from_html(
+        cls, file_path: Path | str
+    ) -> Dict[Set[Path], Set[Path]]:
+        matches: List[str] = cls.find_all_img_paths_in_html(file_path)
+        return_dict = cls.get_dict_of_img_paths(matches, file_path.parent.resolve())
+        return return_dict
--- src/test/test_onehtml.py
+++ src/test/test_onehtml.py
@@ -11,4 +11,53 @@ class TestOneHTML(unittest.TestCase):
     def setUp(self) -> None:
         self.sample_base_path = Path(__file__).parent / 'sample'
         self.target_html = Path(__file__).parent / 'sample/notes.html'
+        self.img_srcs = [
+            'responsesContactReport.png',
+            'webNav.png',
+            'createCMFClassic.png',
+            'instructionsCreate.png',
+            'noSubTabMAX.png',
+            'createAsEdit.png',
+            'editCMFClassic.png',
+            'pageCrash.png',
+            'makeFieldRequired.png',
+            'correctEditForm.png'
+        ]
+        self.images = {
+            'png': {
+                Path(__file__).parent / 'sample/correctEditForm.png',
+                Path(__file__).parent / 'sample/createAsEdit.png',
+                Path(__file__).parent / 'sample/createCMFClassic.png',
+                Path(__file__).parent / 'sample/editCMFClassic.png',
+                Path(__file__).parent / 'sample/instructionsCreate.png',
+                Path(__file__).parent / 'sample/makeFieldRequired.png',
+                Path(__file__).parent / 'sample/noSubTabMAX.png',
+                Path(__file__).parent / 'sample/pageCrash.png',
+                Path(__file__).parent / 'sample/responsesContactReport.png',
+                Path(__file__).parent / 'sample/webNav.png'
+            },
+            'jpg': set()
+        }
 
+    def test_get_all_imgs_from_folder(self):
+        self.assertEqual(onehtml.get_all_images_in_folder(self.sample_base_path), self.images)
+        self.assertEqual(onehtml.get_all_images_in_folder(self.sample_base_path.as_posix()), self.images)
+        with self.assertRaises(TypeError):
+            onehtml.get_all_images_in_folder(self.target_html)
+
+    def test_find_all_img_paths(self):
+        with self.assertRaises(TypeError):
+            onehtml.find_all_img_paths_in_html(self.sample_base_path)
+        self.assertEqual(onehtml.find_all_img_paths_in_html(self.target_html), self.img_srcs)
+
+    def test_get_dict_from_img_paths(self):
+        self.assertEqual(onehtml.get_dict_of_img_paths(self.img_srcs, self.sample_base_path), self.images)
+
+    def test_get_all_imgs_from_html(self):
+        with self.assertRaises(TypeError):
+            onehtml.get_all_images_from_html(self.sample_base_path)
+        self.assertEqual(onehtml.get_all_images_from_html(self.target_html), self.images)
+        
+
+if __name__ == '__main__':
+    unittest.main()
